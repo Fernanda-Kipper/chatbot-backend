31c4a851e2aa510a71f29289db3fcb34749efbe6
Fernanda <nanda.kipper@gmail.com>
2024-02-25 21:09:56 -0300

    fix: remove System.out

--- src/main/java/com/kipper/museumchatbot/services/FaqService.java
+++ src/main/java/com/kipper/museumchatbot/services/FaqService.java
@@ -14,7 +14,6 @@ public class FaqService {
     public String getAnswer(String question){
         String[] words = question.toLowerCase().split("\\s+");
         List wordsList = Arrays.asList(words).stream().map(String::toLowerCase).toList();
-        System.out.println(wordsList);
 
         for (FaqAnswer entry : faqAnswers.getAnswers()) {
             for (String keyword : entry.getKeywords()) {
